80af9e724a710ccabfc5b3c2caaf4d4f5fdb86c7
mint <mint@mint-ThinkPad-T41p.(none)>
2014-02-12 16:49:48 +0100

    .

--- src/DisplayImage.cpp
+++ src/DisplayImage.cpp
@@ -3,7 +3,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include <iostream>
+
 using namespace cv;
+using namespace std;
+
+Mat loadAsGray( char* filename ) {
+	Mat src, gray;
+	src = imread( filename );
+	if( !src.data )
+	{ return gray; }
+
+	// Convert it to gray
+	cvtColor( src, gray, CV_RGB2GRAY );
+
+	return gray;
+}
 
 /** @function main */
 int main( int argc, char** argv )
@@ -17,40 +32,70 @@ int main( int argc, char** argv )
   int delta = 0;
   int ddepth = CV_16S;
   int thresh = 150;
+  size_t L = 1;
 
   // Load an image
-  const char* filename = argc >= 2 ? argv[1] : "./aquarium.png";
-  src = imread( filename );
-  if( !src.data )
-  { return -1; }
+  //char* filename = argc >= 2 ? argv[1] : "./aquarium.png";
+  //src = imread( filename );
+  //if( !src.data )
+  //{ return -1; }
 
   //GaussianBlur( src, src, Size(3,3), 0, 0, BORDER_DEFAULT );
 
   // Convert it to gray
-  cvtColor( src, src_gray, CV_RGB2GRAY );
+  //cvtColor( src, src_gray, CV_RGB2GRAY );
 
-  /// Create window
-  namedWindow( window_name, CV_WINDOW_AUTOSIZE );
-  namedWindow( window_name2, CV_WINDOW_AUTOSIZE );
+  src_gray = loadAsGray( argv[1] );
 
   // Sobel Filter
   /// Generate grad_x and grad_y
   Mat grad_x, grad_y;
   Mat abs_grad_x, abs_grad_y;
   /// Gradient X
-  //Scharr( src_gray, grad_x, ddepth, 1, 0, scale, delta, BORDER_DEFAULT );
   Sobel( src_gray, grad_x, ddepth, 1, 0, 3, scale, delta, BORDER_DEFAULT );
   convertScaleAbs( grad_x, abs_grad_x );
   /// Gradient Y
-  //Scharr( src_gray, grad_y, ddepth, 0, 1, scale, delta, BORDER_DEFAULT );
   Sobel( src_gray, grad_y, ddepth, 0, 1, 3, scale, delta, BORDER_DEFAULT );
   convertScaleAbs( grad_y, abs_grad_y );
   /// Total Gradient (approximate)
   addWeighted( abs_grad_x, 0.5, abs_grad_y, 0.5, 0, grad );
 
-  // Treshold
+  // threshold
   threshold( grad, thresh_grad, thresh, 255, 3);
 
+
+  // create accumulator array
+  //int w = thresh_grad.cols;
+  //int h = thresh_grad.rows;
+  //double hough_h = ((sqrt(2.0) * (double)(h>w?h:w) / 2.0 ));
+  //double accu_h = hough_h * 2.0;
+  //double accu_w = 180;
+
+  vector<Vec4i> accu;
+
+   // for each pixel
+  for( size_t y = 0; y < thresh_grad.rows - 1; y++ ) {
+	  for( size_t x = 0; x < thresh_grad.rows - 1; x++ ) {
+
+		  if(thresh_grad.at<uint>(x,y) > 0) {
+
+		  	  // for each theta
+		  	  for( size_t t = 0; t < 180 - 1; t++ ) {
+				  //foreach(point on Gerade(x,y,t) {
+		  		  	  //if (Bedingungen == TRUE) {
+		  		  	  	  //accumulate(accu,x,y,t,L);
+		  		  	  //}
+		  	  	  //}
+		  	  }
+
+		  }
+
+	    }
+  }
+
+  /// Create window
+  namedWindow( window_name, CV_WINDOW_AUTOSIZE );
+  namedWindow( window_name2, CV_WINDOW_AUTOSIZE );
   // Show images
   imshow( window_name, grad );
   imshow( window_name2, thresh_grad );
@@ -58,20 +103,4 @@ int main( int argc, char** argv )
   waitKey(0);
 
   return 0;
-  }
-
-
-#include "opencv2/highgui/highgui.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
-
-#include <iostream>
-
-using namespace cv;
-using namespace std;
-
-void help()
-{
- cout << "\nThis program demonstrates line finding with the Hough transform.\n"
-         "Usage:\n"
-         "./houghlines <image_name>, Default is pic1.jpg\n" << endl;
 }
