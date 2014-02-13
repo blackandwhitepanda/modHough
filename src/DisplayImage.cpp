13750d41d76f25b1648267f2cab3640b7bcf71ff
mint <mint@mint-ThinkPad-T41p.(none)>
2014-02-13 17:11:16 +0100

    put the sobel filter into a seperate function more comments

--- src/DisplayImage.cpp
+++ src/DisplayImage.cpp
@@ -20,47 +20,49 @@ Mat loadAsGray( char* filename ) {
 	return gray;
 }
 
+Mat sobelMagnitude( Mat image ) {
+	// Sobel Filter
+	/// Generate grad_x and grad_y
+	Mat grad_x, grad_y;
+	Mat abs_grad_x, abs_grad_y;
+	Mat grad;
+	int scale = 1;
+	int delta = 0;
+	int ddepth = CV_16S;
+
+	/// Gradient X
+	Sobel( image, grad_x, ddepth, 1, 0, 3, scale, delta, BORDER_DEFAULT );
+	convertScaleAbs( grad_x, abs_grad_x );
+	/// Gradient Y
+	Sobel( image, grad_y, ddepth, 0, 1, 3, scale, delta, BORDER_DEFAULT );
+	convertScaleAbs( grad_y, abs_grad_y );
+	/// Total Gradient (approximate)
+	addWeighted( abs_grad_x, 0.5, abs_grad_y, 0.5, 0, grad );
+
+	return grad;
+}
+
 /** @function main */
 int main( int argc, char** argv )
 {
-
-  Mat src, src_gray;
+  Mat src_gray;
   Mat grad, thresh_grad;
   char* window_name = "Sobel";
   char* window_name2 = "Threshold";
-  int scale = 1;
-  int delta = 0;
-  int ddepth = CV_16S;
-  int thresh = 150;
+
+  int thresh = 100;
   size_t L = 1;
 
-  // Load an image
+  // Load an image as grayscale matrix
   //char* filename = argc >= 2 ? argv[1] : "./aquarium.png";
-  //src = imread( filename );
-  //if( !src.data )
-  //{ return -1; }
-
-  //GaussianBlur( src, src, Size(3,3), 0, 0, BORDER_DEFAULT );
-
-  // Convert it to gray
-  //cvtColor( src, src_gray, CV_RGB2GRAY );
-
   src_gray = loadAsGray( argv[1] );
 
-  // Sobel Filter
-  /// Generate grad_x and grad_y
-  Mat grad_x, grad_y;
-  Mat abs_grad_x, abs_grad_y;
-  /// Gradient X
-  Sobel( src_gray, grad_x, ddepth, 1, 0, 3, scale, delta, BORDER_DEFAULT );
-  convertScaleAbs( grad_x, abs_grad_x );
-  /// Gradient Y
-  Sobel( src_gray, grad_y, ddepth, 0, 1, 3, scale, delta, BORDER_DEFAULT );
-  convertScaleAbs( grad_y, abs_grad_y );
-  /// Total Gradient (approximate)
-  addWeighted( abs_grad_x, 0.5, abs_grad_y, 0.5, 0, grad );
+  // get the sobel gradient magnitude matrix
+  grad = sobelMagnitude( src_gray );
 
-  // threshold
+  // implement a threshold to satisfy the condition that in the
+  // following hough transform accumulation only takes place when
+  // the magnitude of the gradient is greater than a threshold.
   threshold( grad, thresh_grad, thresh, 255, 3);
 
 
@@ -75,7 +77,7 @@ int main( int argc, char** argv )
 
    // for each pixel
   for( size_t y = 0; y < thresh_grad.rows - 1; y++ ) {
-	  for( size_t x = 0; x < thresh_grad.cols - 1; x++ ) {
+	  for( size_t x = 0; x < thresh_grad.rows - 1; x++ ) {
 
 		  if(thresh_grad.at<uint>(x,y) > 0) {
 
